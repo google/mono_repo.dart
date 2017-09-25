683c55833797b2c08acdc69db59c2d93ef864ada
Kevin Moore <kevmoo@google.com>
2017-09-25 14:42:35 -0700

    Add a test

--- test/multi_repo_test.dart
+++ test/multi_repo_test.dart
@@ -1,7 +1,28 @@
 // Copyright (c) 2017, Kevin Moore. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-//import 'package:mono_repo/mono_repo.dart';
-//import 'package:test/test.dart';
+import 'package:test/test.dart';
+import 'package:test_process/test_process.dart';
 
-void main() {}
+void main() {
+  test('pub get gets dependencies', () async {
+    var process = await TestProcess.start('pub', ['run', 'mono_repo']);
+
+    var output = await process.stdoutStream().join('\n');
+    expect(output, '''Manage multiple packages in one source repository.
+
+Usage: mono_repo <command> [arguments]
+
+Global options:
+-h, --help    Print this usage information.
+
+Available commands:
+  check   Check the state of the repository.
+  help    Display help information for mono_repo.
+  init    Initialize a new repository.
+
+Run "mono_repo help <command>" for more information about a command.''');
+
+    await process.shouldExit(0);
+  });
+}
