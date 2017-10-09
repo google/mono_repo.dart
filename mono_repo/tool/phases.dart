31877df64bcf5e04dd4cb04b969c7f463cfd2ff1
Kevin Moore <kevmoo@users.noreply.github.com>
2017-10-09 12:53:13 -0700

    Include copyright in generated files (#12)

--- mono_repo/tool/phases.dart
+++ mono_repo/tool/phases.dart
@@ -10,7 +10,15 @@ final List<BuildAction> phases = [
   new BuildAction(
       new PartBuilder(const [
         const JsonSerializableGenerator(),
-      ]),
+      ], header: _dartCopyright),
       'mono_repo',
       inputs: const ['lib/src/*'])
 ];
+
+final _dartCopyright =
+    '''// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+$defaultFileHeader
+''';
