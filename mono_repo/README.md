ce856c28b9633ff53ec0775e7c1bf073bcd2295b
Kevin Moore <kevmoo@google.com>
2018-08-28 10:23:36 -0700

    misc: add example to readme + tests, prepare to release v1
    Towards https://github.com/dart-lang/mono_repo/issues/69

--- mono_repo/README.md
+++ mono_repo/README.md
@@ -1,12 +1,24 @@
 Manage multiple [Dart packages] within a single repository.
 
-### Installation
+## Installation
 
 ```console
 > pub global activate mono_repo
 ```
 
-### Usage
+## Running
+
+```console
+> pub global run mono_repo
+```
+
+Or, once you've [setup your PATH]:
+
+```console
+> mono_repo
+```
+
+Prints the following help message: 
 
 ```
 Manage multiple packages in one source repository.
@@ -27,4 +39,43 @@ Available commands:
 Run "mono_repo help <command>" for more information about a command.
 ```
 
+## Configuration
+
+To configure a package directory to be included it must contain a
+`mono_pkg.yaml` file (along with the normal `pubspec.yaml` file).
+
+You can use an empty `mono_pkg.yaml` file to enable the `check` and `pub`
+commands. 
+
+To enable `travis` and `presubmit`, you must populate `mono_pkg.yaml` with
+details on how you'd like tests to be run.
+
+### `mono_pkg.yaml` example
+
+```yaml
+# This key is required. It specifies the Dart SDKs your tests will run under
+# You can provide one or more value.
+# See https://docs.travis-ci.com/user/languages/dart#choosing-dart-versions-to-test-against
+# for valid values
+dart:
+ - dev
+
+stages:
+  # Register two jobs to run under the `analyze` stage.
+  - analyze:
+    - dartanalyzer
+    - dartfmt
+  - unit_test:
+    - test
+```
+
+Running `mono_repo travis` in the root directory generates two files:
+`.travis.yml` and `tool/travis.sh`.
+
+Look at these repositories for examples of `mono_repo` usage:
+
+* https://github.com/dart-lang/build
+* https://github.com/dart-lang/json_serializable
+
 [Dart packages]: https://www.dartlang.org/guides/libraries/create-library-packages
+[setup your PATH]: (https://www.dartlang.org/tools/pub/cmd/pub-global#running-a-script-from-your-path)
---
+++ mono_repo/test/readme_test.dart
@@ -0,0 +1,138 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:test_descriptor/test_descriptor.dart' as d;
+
+import 'package:mono_repo/src/package_config.dart';
+
+import 'shared.dart';
+
+// TODO(kevmoo): validate `mono_repo --help` output, too!
+
+void main() {
+  test('validate readme content', () {
+    var readmeContent = File('README.md').readAsStringSync();
+    expect(readmeContent, contains(_pkgConfig));
+  });
+
+  test('validate readme example output', () async {
+    await d.dir('sub_pkg', [
+      d.file(monoPkgFileName, _pkgConfig),
+      d.file('pubspec.yaml', '''
+name: sub_pkg
+''')
+    ]).create();
+
+    await testGenerateTravisConfig();
+
+    await d.dir('.', [
+      d.file(travisFileName, _travisYml),
+      d.file(travisShPath, _travisSh)
+    ]).validate();
+  });
+}
+
+final _pkgConfig = r'''
+# This key is required. It specifies the Dart SDKs your tests will run under
+# You can provide one or more value.
+# See https://docs.travis-ci.com/user/languages/dart#choosing-dart-versions-to-test-against
+# for valid values
+dart:
+ - dev
+
+stages:
+  # Register two jobs to run under the `analyze` stage.
+  - analyze:
+    - dartanalyzer
+    - dartfmt
+  - unit_test:
+    - test
+''';
+
+final _travisYml = r'''
+# Created with https://github.com/dart-lang/mono_repo
+language: dart
+
+jobs:
+  include:
+    - stage: analyze
+      name: "SDK: dev - DIR: sub_pkg - TASKS: dartanalyzer ."
+      script: ./tool/travis.sh dartanalyzer
+      env: PKG="sub_pkg"
+      dart: dev
+    - stage: analyze
+      name: "SDK: dev - DIR: sub_pkg - TASKS: dartfmt -n --set-exit-if-changed ."
+      script: ./tool/travis.sh dartfmt
+      env: PKG="sub_pkg"
+      dart: dev
+    - stage: unit_test
+      name: "SDK: dev - DIR: sub_pkg - TASKS: pub run test"
+      script: ./tool/travis.sh test
+      env: PKG="sub_pkg"
+      dart: dev
+
+stages:
+  - analyze
+  - unit_test
+
+# Only building master means that we don't run two builds for each pull request.
+branches:
+  only:
+    - master
+
+cache:
+  directories:
+    - "$HOME/.pub-cache"
+''';
+
+final _travisSh = r'''
+#!/bin/bash
+# Created with https://github.com/dart-lang/mono_repo
+
+if [ -z "$PKG" ]; then
+  echo -e '\033[31mPKG environment variable must be set!\033[0m'
+  exit 1
+fi
+
+if [ "$#" == "0" ]; then
+  echo -e '\033[31mAt least one task argument must be provided!\033[0m'
+  exit 1
+fi
+
+pushd $PKG
+pub upgrade || exit $?
+
+EXIT_CODE=0
+
+while (( "$#" )); do
+  TASK=$1
+  case $TASK in
+  dartanalyzer) echo
+    echo -e '\033[1mTASK: dartanalyzer\033[22m'
+    echo -e 'dartanalyzer .'
+    dartanalyzer . || EXIT_CODE=$?
+    ;;
+  dartfmt) echo
+    echo -e '\033[1mTASK: dartfmt\033[22m'
+    echo -e 'dartfmt -n --set-exit-if-changed .'
+    dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
+    ;;
+  test) echo
+    echo -e '\033[1mTASK: test\033[22m'
+    echo -e 'pub run test'
+    pub run test || EXIT_CODE=$?
+    ;;
+  *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+    EXIT_CODE=1
+    ;;
+  esac
+
+  shift
+done
+
+exit $EXIT_CODE
+''';
