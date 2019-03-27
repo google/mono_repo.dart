e93e43ddd5a8ec46d03aad424a4a2d3825773422
Kevin Moore <kevmoo@google.com>
2019-03-27 01:26:10 -0700

    test: case where dart config is ignored

--- mono_repo/test/travis_command_test.dart
+++ mono_repo/test/travis_command_test.dart
@@ -287,6 +287,107 @@ name: pkg_a
         ));
   });
 
+  test('top-level `dart` key value is no-op with group overrides', () async {
+    await d.dir('pkg_a', [
+      d.file(monoPkgFileName, r'''
+dart:
+- no_op_dart_var
+
+stages:
+- analyzer_and_format:
+  - group:
+    - dartfmt
+    - dartanalyzer: --fatal-warnings --fatal-infos .
+    dart: [dev]
+  - group:
+    - dartfmt
+    - dartanalyzer: --fatal-warnings .
+    dart: [2.1.1]
+'''),
+      d.file('pubspec.yaml', '''
+name: pkg_a
+      ''')
+    ]).create();
+
+    await testGenerateTravisConfig();
+    await d.file(travisFileName, r'''
+# Created with package:mono_repo v1.2.3
+language: dart
+
+jobs:
+  include:
+    - stage: analyzer_and_format
+      name: "SDK: dev - DIR: pkg_a - TASKS: [dartfmt -n --set-exit-if-changed ., dartanalyzer --fatal-warnings --fatal-infos .]"
+      script: ./tool/travis.sh dartfmt dartanalyzer_0
+      env: PKG="pkg_a"
+      dart: dev
+    - stage: analyzer_and_format
+      name: "SDK: 2.1.1 - DIR: pkg_a - TASKS: [dartfmt -n --set-exit-if-changed ., dartanalyzer --fatal-warnings .]"
+      script: ./tool/travis.sh dartfmt dartanalyzer_1
+      env: PKG="pkg_a"
+      dart: "2.1.1"
+
+stages:
+  - analyzer_and_format
+
+# Only building master means that we don't run two builds for each pull request.
+branches:
+  only:
+    - master
+
+cache:
+  directories:
+    - "$HOME/.pub-cache"
+''').validate();
+    await d.file(travisShPath, r'''
+#!/bin/bash
+# Created with package:mono_repo v1.2.3
+
+if [ -z "$PKG" ]; then
+  echo -e '\033[31mPKG environment variable must be set!\033[0m'
+  exit 1
+fi
+
+if [ "$#" == "0" ]; then
+  echo -e '\033[31mAt least one task argument must be provided!\033[0m'
+  exit 1
+fi
+
+pushd $PKG
+pub upgrade || exit $?
+
+EXIT_CODE=0
+
+while (( "$#" )); do
+  TASK=$1
+  case $TASK in
+  dartanalyzer_0) echo
+    echo -e '\033[1mTASK: dartanalyzer_0\033[22m'
+    echo -e 'dartanalyzer --fatal-warnings --fatal-infos .'
+    dartanalyzer --fatal-warnings --fatal-infos . || EXIT_CODE=$?
+    ;;
+  dartanalyzer_1) echo
+    echo -e '\033[1mTASK: dartanalyzer_1\033[22m'
+    echo -e 'dartanalyzer --fatal-warnings .'
+    dartanalyzer --fatal-warnings . || EXIT_CODE=$?
+    ;;
+  dartfmt) echo
+    echo -e '\033[1mTASK: dartfmt\033[22m'
+    echo -e 'dartfmt -n --set-exit-if-changed .'
+    dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
+    ;;
+  *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+    EXIT_CODE=1
+    ;;
+  esac
+
+  shift
+done
+
+exit $EXIT_CODE
+''').validate();
+  });
+
   group('mono_repo.yaml', () {
     Future populateConfig(String monoRepoContent) async {
       await d.file('mono_repo.yaml', monoRepoContent).create();
