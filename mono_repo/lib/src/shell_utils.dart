32d6218c721a8007b64a706e2642be1a870ec4fd
Kevin Moore <kevmoo@google.com>
2019-04-04 07:53:10 -0700

    nit in utility doc comment

--- mono_repo/lib/src/shell_utils.dart
+++ mono_repo/lib/src/shell_utils.dart
@@ -3,7 +3,7 @@ import 'package:io/ansi.dart';
 /// Safely escape everything:
 /// 1 - use single quotes.
 /// 2 - if there is a single quote in the string
-///     2.1 end the before the single quote
+///     2.1 end the string before the single quote
 ///     2.2 echo the single quote escaped
 ///     2.3 continue the string
 ///
