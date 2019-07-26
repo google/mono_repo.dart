2b898025d6343ddd4fe1d2f2632d37180a826377
Kevin Moore <kevmoo@google.com>
2019-07-25 19:06:29 -0700

    rebuild with latest json_seria

--- mono_repo/lib/src/raw_config.g.dart
+++ mono_repo/lib/src/raw_config.g.dart
@@ -10,24 +10,27 @@ RawConfig _$RawConfigFromJson(Map json) {
   return $checkedNew('RawConfig', json, () {
     $checkKeys(json, allowedKeys: const ['dart', 'stages', 'cache']);
     final val = RawConfig(
-        $checkedConvert(json, 'dart',
-            (v) => (v as List)?.map((e) => e as String)?.toList()),
-        $checkedConvert(
-            json,
-            'stages',
-            (v) => (v as List)
-                ?.map((e) => e == null ? null : RawStage.fromJson(e as Map))
-                ?.toList()),
-        $checkedConvert(json, 'cache',
-            (v) => v == null ? null : RawCache.fromJson(v as Map)));
+      $checkedConvert(
+          json, 'dart', (v) => (v as List)?.map((e) => e as String)?.toList()),
+      $checkedConvert(
+          json,
+          'stages',
+          (v) => (v as List)
+              ?.map((e) => e == null ? null : RawStage.fromJson(e as Map))
+              ?.toList()),
+      $checkedConvert(
+          json, 'cache', (v) => v == null ? null : RawCache.fromJson(v as Map)),
+    );
     return val;
   }, fieldKeyMap: const {'sdks': 'dart'});
 }
 
 RawCache _$RawCacheFromJson(Map json) {
   return $checkedNew('RawCache', json, () {
-    final val = RawCache($checkedConvert(json, 'directories',
-        (v) => (v as List)?.map((e) => e as String)?.toList()));
+    final val = RawCache(
+      $checkedConvert(json, 'directories',
+          (v) => (v as List)?.map((e) => e as String)?.toList()),
+    );
     return val;
   });
 }
