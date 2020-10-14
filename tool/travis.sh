549000102380105f04975004fc0e6531d6dd7cc5
Kevin Moore <kevmoo@google.com>
2020-10-14 13:28:41 -0700

    Simplify test setup
    Build is already verified
    Don't use build_runner to run tests

--- tool/travis.sh
+++ tool/travis.sh
@@ -59,12 +59,8 @@ for PKG in ${PKGS}; do
       cd ../ && dart mono_repo/bin/mono_repo.dart travis --validate || EXIT_CODE=$?
       ;;
     command_1)
-      echo 'pub run build_runner build test --delete-conflicting-outputs'
-      pub run build_runner build test --delete-conflicting-outputs || EXIT_CODE=$?
-      ;;
-    command_2)
-      echo 'pub run build_runner test --delete-conflicting-outputs -- -P presubmit'
-      pub run build_runner test --delete-conflicting-outputs -- -P presubmit || EXIT_CODE=$?
+      echo 'pub run test -P presubmit'
+      pub run test -P presubmit || EXIT_CODE=$?
       ;;
     dartanalyzer_0)
       echo 'dartanalyzer --fatal-infos --fatal-warnings .'
