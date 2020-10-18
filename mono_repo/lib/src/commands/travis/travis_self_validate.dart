597b7f329284ff570e239398098c0e458387c27a
Kevin Moore <kevmoo@google.com>
2020-10-18 13:48:22 -0700

    DRY up code that generates self-validate script

--- mono_repo/lib/src/commands/travis/travis_self_validate.dart
+++ mono_repo/lib/src/commands/travis/travis_self_validate.dart
@@ -1,17 +1,11 @@
 import '../../version.dart';
+import 'shared.dart';
 
 String generateSelfValidate() => '''
 #!/bin/bash
-# Created with package:mono_repo v$packageVersion
-
+${createdWith()}
 # Support built in commands on windows out of the box.
-function pub {
-       if [[ \$TRAVIS_OS_NAME == "windows" ]]; then
-        command pub.bat "\$@"
-    else
-        command pub "\$@"
-    fi
-}
+${dartCommandContent('pub')}
 
 set -v -e
 pub global activate mono_repo $packageVersion
