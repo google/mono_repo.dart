808af11a4e01fb89d4bd9e6b429d2dd1dc2cb1b9
Kevin Moore <kevmoo@google.com>
2020-10-19 13:10:11 -0700

    merge

--- mono_repo/test/travis_command_test.dart
+++ mono_repo/test/travis_command_test.dart
@@ -1219,7 +1219,7 @@ EXIT_CODE=0
 
 for PKG in ${PKGS}; do
   echo -e "PKG: ${PKG}"
-  pushd "${PKG}" || exit $?
+  pushd "${PKG}" > /dev/null || exit $?
 
   PUB_EXIT_CODE=0
   pub upgrade --no-precompile || PUB_EXIT_CODE=$?
@@ -1227,7 +1227,8 @@ for PKG in ${PKGS}; do
   if [[ ${PUB_EXIT_CODE} -ne 0 ]]; then
     EXIT_CODE=1
     echo -e 'pub upgrade failed'
-    popd
+    popd > /dev/null
+    echo
     continue
   fi
 
@@ -1258,7 +1259,8 @@ for PKG in ${PKGS}; do
     esac
   done
 
-  popd
+  popd > /dev/null
+  echo
 done
 
 exit ${EXIT_CODE}
