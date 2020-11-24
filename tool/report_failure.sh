83da1f3aca7e66e9ce96cb3a275c5340179d4181
Jacob MacDonald <jakemac@google.com>
2020-11-24 09:09:54 -0800

    and fix the name in the sh script also

--- tool/report_failure.sh
+++ tool/report_failure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -H "Content-Type: application/json" -X POST -d \
   "{'text':'Build failed! ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}'}" \
-  "${CHAT_HOOK_URI}"
+  "${CHAT_WEBHOOK_URI}"
