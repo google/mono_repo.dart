9e98a69eee6b93bd39ba98eed657e71b72457f2d
Ashraff Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2020-12-11 15:41:40 -0500

    Only add mono_pkg.yaml to directories that have a pubspec.yaml

--- mono_repo/lib/src/commands/init.dart
+++ mono_repo/lib/src/commands/init.dart
@@ -5,15 +5,54 @@
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
+import 'package:io/ansi.dart';
 import 'package:path/path.dart' as p;
+import 'package:pubspec_parse/pubspec_parse.dart';
 
 const _pubspecFileName = 'pubspec.yaml';
 const _pkgCfgFileName = 'mono_pkg.yaml';
 const _repoCfgFileName = 'mono_repo.yaml';
 const _recursiveScanFlag = 'recursive';
 
-const _commentText =
-    '# See with https://github.com/dart-lang/mono_repo for details on this file\n';
+const _repoCfgContents = r"""
+# See with https://github.com/dart-lang/mono_repo for details on this file
+self_validate: analyze_and_format
+
+github:
+  # Setting just `cron` keeps the defaults for `push` and `pull_request`
+  cron: '0 0 * * 0'
+  on_completion:
+    - name: "Notify failure"
+      runs-on: ubuntu-latest
+      # Run only if other jobs have failed and this is a push or scheduled build.
+      if: (github.event_name == 'push' || github.event_name == 'schedule') && failure()
+      steps:
+        - run: >
+            curl -H "Content-Type: application/json" -X POST -d \
+              "{'text':'Build failed! ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}'}" \
+              "${CHAT_WEBHOOK_URL}"
+          env:
+            CHAT_WEBHOOK_URL: ${{ secrets.BUILD_AND_TEST_TEAM_CHAT_WEBHOOK_URL }}
+
+merge_stages:
+- analyze_and_format
+""";
+
+const _pkgCfgContents = r"""
+dart:
+- dev
+- stable
+
+os:
+- linux
+
+stages:
+- analyze_and_format:
+  - dartanalyzer: --fatal-infos --fatal-warnings .
+  - dartfmt: sdk
+- unit_test:
+  - test:
+""";
 
 class InitCommand extends Command<void> {
   @override
@@ -37,25 +76,22 @@ void configureDirectory(String rootDir,
   if (currentDir == rootDir) {
     final repoCfgPath = p.join(rootDir, _repoCfgFileName);
     if (!File(repoCfgPath).existsSync()) {
-      File(repoCfgPath).writeAsStringSync(_commentText);
-      print('Added $_repoCfgFileName to $rootDir');
+      File(repoCfgPath).writeAsStringSync(_repoCfgContents);
+      print(styleBold.wrap('Added $_repoCfgFileName to $rootDir'));
     } else {
-      print('$_repoCfgFileName already present in $rootDir. Exiting...');
-      return;
+      print(yellow.wrap('$_repoCfgFileName already present in $rootDir.'));
     }
   } else {
     final pkgCfgPath = p.join(currentDir, _pkgCfgFileName);
     final pubspecPath = p.join(currentDir, _pubspecFileName);
 
-    if (!File(pubspecPath).existsSync()) {
-      final pubspecContents = 'name: ${p.basename(currentDir)}\n';
-      File(pubspecPath).writeAsStringSync(pubspecContents);
-      print('Added $_pubspecFileName to $currentDir');
-    }
-
-    if (!File(pkgCfgPath).existsSync()) {
-      File(pkgCfgPath).writeAsStringSync(_commentText);
-      print('Added $_pkgCfgFileName to $currentDir');
+    if (File(pubspecPath).existsSync()) {
+      if (!File(pkgCfgPath).existsSync()) {
+        File(pkgCfgPath).writeAsStringSync(_pkgCfgContents);
+        print(styleBold.wrap('Added $_pkgCfgFileName to $currentDir'));
+      } else {
+        print(yellow.wrap('$_pkgCfgFileName already present in $currentDir'));
+      }
     }
   }
 
--- mono_repo/test/init_command_test.dart
+++ mono_repo/test/init_command_test.dart
@@ -1,26 +1,32 @@
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 import 'package:test_descriptor/test_descriptor.dart' as d;
 
 import 'package:mono_repo/src/commands/init.dart';
 
 void main() {
-  const commentText =
-      '# See with https://github.com/dart-lang/mono_repo for details on this file\n';
+  const repoCfgFileName = 'mono_repo.yaml';
+  const pkgCfgFileName = 'mono_pkg.yaml';
+
   test('scaffold a new mono repo', () async {
-    await d.dir(
-        'top_level', [d.dir('package1', []), d.dir('package2', [])]).create();
-    final rootDirectory = '${d.sandbox}/top_level';
-    scaffold(rootDirectory, false);
     await d.dir('top_level', [
-      d.file('mono_repo.yaml', commentText),
-      d.dir('package1', [
-        d.file('pubspec.yaml', 'name: package1\n'),
-        d.file('mono_pkg.yaml', commentText)
-      ]),
-      d.dir('package2', [
-        d.file('pubspec.yaml', 'name: package2\n'),
-        d.file('mono_pkg.yaml', commentText)
-      ])
-    ]).validate();
+      d.dir('package1', [d.file('pubspec.yaml')]),
+      d.dir('package2', [d.file('pubspec.yaml')])
+    ]).create();
+    final rootDirectory = '${d.sandbox}/top_level';
+    final package1Directory = '${d.sandbox}/top_level/package1';
+    final package2Directory = '${d.sandbox}/top_level/package2';
+
+    final repoCfg = p.join(rootDirectory, repoCfgFileName);
+    final pkg1Cfg = p.join(package1Directory, pkgCfgFileName);
+    final pkg2Cfg = p.join(package2Directory, pkgCfgFileName);
+    scaffold(rootDirectory, false);
+    expect(
+        File(repoCfg).existsSync() &&
+            File(pkg1Cfg).existsSync() &&
+            File(pkg2Cfg).existsSync(),
+        equals(true));
   });
 }
