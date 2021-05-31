64c23583ad98701621dd7d334a9e3db1c6bbdda0
Kevin Moore <kevmoo@google.com>
2021-05-31 10:00:45 -0700

    nits

--- mono_repo/lib/src/commands/github/github_yaml.dart
+++ mono_repo/lib/src/commands/github/github_yaml.dart
@@ -426,7 +426,7 @@ Map<String, dynamic> _cacheEntries(
 
   return {
     'name': 'Cache Pub hosted dependencies',
-    'uses': 'actions/cache@v2.1.5',
+    'uses': 'actions/cache@v2.1.6',
     'with': {
       'path': pubCacheHosted,
       'key': cacheKeyParts.join(';'),
