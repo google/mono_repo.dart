63ed3d06debeac09a82571185159bf6063955a19
Kevin Moore <kevmoo@google.com>
2021-11-14 15:05:59 -0800

    Update blns for text_direction_code_point_in_literal hint

--- mono_repo/test/yaml_write_test.dart
+++ mono_repo/test/yaml_write_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: lines_longer_than_80_chars, text_direction_code_point_in_literal
 
 @Tags(['yaml'])
 import 'dart:convert';
