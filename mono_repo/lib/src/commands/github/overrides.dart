b4af2ae4fc10d88e8b2d66413c2026a589101bc1
Dillon Nys <nydillon@amazon.com>
2022-07-18 06:23:57 -0700

    Add missing license header

--- mono_repo/lib/src/commands/github/overrides.dart
+++ mono_repo/lib/src/commands/github/overrides.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 abstract class GitHubActionOverrides {
   /// The step's identifier, which can be used to refer to the step and its
   /// outputs in the [ifContent] property of this and other steps.
