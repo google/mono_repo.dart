19af997e81abc5aa12cd67295ef0d9f426f1de06
Parker Lougheed <parlough@gmail.com>
2023-10-14 09:29:18 -0500

    Update to dart-lang/setup-dart@1.6

--- mono_repo/lib/src/commands/github/action_versions.dart
+++ mono_repo/lib/src/commands/github/action_versions.dart
@@ -7,7 +7,7 @@
 /// To regenerate it, run the `tool/generate_action_versions.dart` script.
 
 const actionsCacheVersion = '704facf57e6136b1bc63b828d79edcd491f0ee84';
-const dartLangSetupDartVersion = '8a4b97ea2017cc079571daec46542f76189836b1';
+const dartLangSetupDartVersion = 'b64355ae6ca0b5d484f0106a033dd1388965d06d';
 const actionsCheckoutVersion = '8ade135a41bc03ea155e62e844d188df1ea18608';
 const subositoFlutterActionVersion = '48cafc24713cca54bbe03cdc3a423187d413aafa';
 const coverallsappGithubActionVersion = 'master';
